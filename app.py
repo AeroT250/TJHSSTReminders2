9a295bb39bb06c0e7021f3af3aaba78c510d3322
AeroT250 <61627862+AeroT250@users.noreply.github.com>
2020-10-26 17:20:09 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,21 +1,19 @@
 #Python libraries that we need to import for our bot
 import random
-import datetime
-import os
 from flask import Flask, request
 from pymessenger.bot import Bot
-
+import os 
 app = Flask(__name__)
-ACCESS_TOKEN = os.environ['ACCESS_TOKEN']
-VERIFY_TOKEN = os.environ['VERIFY_TOKEN']
-bot = Bot(ACCESS_TOKEN)
+ACCESS_TOKEN = 'ACCESS_TOKEN'   #ACCESS_TOKEN = os.environ['ACCESS_TOKEN']
+VERIFY_TOKEN = 'VERIFY_TOKEN'   #VERIFY_TOKEN = os.environ['VERIFY_TOKEN']
+bot = Bot (ACCESS_TOKEN)
 
-#We will receive messages that Facebook sends our bot at this endpoint
+#We will receive messages that Facebook sends our bot at this endpoint 
 @app.route("/", methods=['GET', 'POST'])
 def receive_message():
     if request.method == 'GET':
         """Before allowing people to message your bot, Facebook has implemented a verify token
-        that confirms all requests that your bot receives came from Facebook."""
+        that confirms all requests that your bot receives came from Facebook.""" 
         token_sent = request.args.get("hub.verify_token")
         return verify_fb_token(token_sent)
     #if the request was not get, it must be POST and we can just proceed with sending a message back to user
@@ -35,13 +33,12 @@ def receive_message():
                 if message['message'].get('attachments'):
                     response_sent_nontext = get_message()
                     send_message(recipient_id, response_sent_nontext)
-
     return "Message Processed"
 
 
 def verify_fb_token(token_sent):
     #take token sent by facebook and verify it matches the verify token you sent
-    #if they match, allow the request, else return an error
+    #if they match, allow the request, else return an error 
     if token_sent == VERIFY_TOKEN:
         return request.args.get("hub.challenge")
     return 'Invalid verification token'
@@ -49,45 +46,9 @@ def verify_fb_token(token_sent):
 
 #chooses a random message to send to the user
 def get_message():
-    dayofweek = datetime.datetime.today().weekday()
-    if(dayofweek == 1):
-        return "Hi! Thanks for checking in. Today is Tuesday, a blue day. This means that you have periods" \
-               "1, 2, 3, and 4, with a break between periods 2 and 3. You don't have to fill out any special" \
-               "form today. Here are the times when each class starts: 1st pd is from 8:40 AM - 10:05 AM. 2nd period" \
-               "is from 10:25 - 11:50. You have a lunch break from 12:00 PM - 12:40 PM. Pd 3 is from 12:50 PM" \
-               "- 2:15 PM. Finally, your last class of the day, pd 4, is from 2:35 PM - 4:00 PM. Have a great day!"
-    if(dayofweek == 2):
-        return "Hi! Thanks for checking in. Today is Wednesday, a red day. This means that you have periods" \
-               "5, 6, 7, and 8 (Clubs!), with a break between periods 6 and 7. You don't need to fill out any " \
-               "forms today. Here are the times for each class: 1st pd is from 8:40 AM - 10:05 AM. 2nd period" \
-               "is from 10:25 - 11:50. You have a lunch break from 12:00 PM - 12:40 PM. Pd 3 is from 12:50 PM" \
-               "- 2:15 PM. Then, the first 8th period block is from 2:30 PM - 3:10 PM. To end the day, we have " \
-               "8th period B block, from 3:20 PM - 4:00 PM. Have an amazing day! "
-    if(dayofweek == 3):
-        return "Hi! Thanks for checking in. Today is Thursday, a blue day. This means that you have periods" \
-               "1, 2, 3, and 4, with a break between periods 2 and 3. You don't have to fill out any special" \
-               "form today. Here are the times when each class starts: 1st pd is from 8:40 AM - 10:05 AM. 2nd period" \
-               "is from 10:25 - 11:50. You have a lunch break from 12:00 PM - 12:40 PM. Pd 3 is from 12:50 PM" \
-               "- 2:15 PM. Finally, your last class of the day, pd 4, is from 2:35 PM - 4:00 PM. Have a great day!"
-    if(dayofweek == 4):
-        return "Hi! Thanks for checking in. Today is Wednesday, a red day. This means that you have periods" \
-               "5, 6, 7, and 8 (Clubs!), with a break between periods 6 and 7. You don't need to fill out any " \
-               "forms today. Here are the times for each class: 1st pd is from 8:40 AM - 10:05 AM. 2nd period" \
-               "is from 10:25 - 11:50. You have a lunch break from 12:00 PM - 12:40 PM. Pd 3 is from 12:50 PM" \
-               "- 2:15 PM. Then, the first 8th period block is from 2:30 PM - 3:10 PM. To end the day, we have " \
-               "8th period B block, from 3:20 PM - 4:00 PM. Have an amazing day! "
-    if(dayofweek == 5):
-        return "It's the weekend (Saturday)! Time to relax, read a book, play with a pet, or let's be honest, spend 8 hours a " \
-               "day doing homework. Make sure to schedule your time to include plenty of breaks, and if you have time," \
-               "be sure to take a day off just to relax. See you next week!"
-    if (dayofweek == 6):
-        return "It's the weekend (Sunday)! Time to relax, read a book, play with a pet, or let's be honest, spend 8 hours a " \
-               "day doing homework. Make sure to schedule your time to include plenty of breaks, and if you have time," \
-               "be sure to take a day off just to relax. See you next week!"
-    if(dayofweek == 0):
-        return "Oh boy: it's monday. At least we have a 3 day weekend! Today is pretty much like Saturday or Sunday, but" \
-               "be sure to fill out the Monday Attendance Form so you don't get marked as absent. " \
-               "Here's the link: https://tinyurl.com/monday-check-in-2020"
+    sample_responses = ["You are stunning!", "We're proud of you.", "Keep on being you!", "We're greatful to know you :)"]
+    # return selected item to the user
+    return random.choice(sample_responses)
 
 #uses PyMessenger to send response to user
 def send_message(recipient_id, response):
